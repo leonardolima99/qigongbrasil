0bb576a09fcb0f73bdefe16f540652e2798a5a1b
Leonardo Lima Coelho <dev.leonardolima@gmail.com>
2022-12-07 21:29:50 -0400

    feat: get articles from blog

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
+/**
+ * Pensar em uma solução para nõa buscar artigos já baixados.
+ * Talvez quando usar um banco de dados, fique mais simples.
+ */
+
 import axios from 'axios';
 import {load} from 'cheerio';
 import fs from 'fs';
+import { setTimeout } from 'timers/promises';
+
+import { slugify, getPage, handleFileJSON } from './src/utils';
 
 const BASE_URL = 'https://qigongbrasil.blogspot.com'
 const SITE_MAP = `${BASE_URL}/sitemap.xml`
-const getPage = async () => {
-  return await axios.get(SITE_MAP)
-}
-const getItems = (html) => {
-  const $ = load(html, {xmlMode: true});
-  const articles = [];
-  $('url').each((i, element) => {
-    articles.push({
-      url: $('loc', element).text(),
-      updated: new Date($('lastmod', element).text())
+
+const getItems = async (html, xml) => {
+  const $ = load(html, {xmlMode: xml});
+  if (xml) {
+    const articles = [];
+    $('url').each((i, element) => {
+      articles.push({
+        url: $('loc', element).text(),
+        updated: new Date($('lastmod', element).text())
+      })
     })
-  })
-  handleFile(articles)
-}
-const handleFile = async (content) => {
-  try {
-    const file = await fs.readFileSync('./data/articlesUrl.json');
-    return file
-  } catch (error) {
-    await fs.createWriteStream('./data/articlesUrl.json');
-  } finally {
-    await fs.writeFileSync('./data/articlesUrl.json', JSON.stringify(content))
+    await handleFileJSON('../data/articlesUrl.json', articles)
+  } else {
+    const article = {
+      title: $('.post-title').text(),
+      content: $('.post-body').html()
+    }
+    const slug = slugify(article.title)
+    await handleFileJSON(`../data/articles/${slug}.json`, article)
   }
 }
-const sitemap = await getPage()
-getItems(sitemap.data);
+
+const getArticle = async () => {
+  const file = JSON.parse(await fs.readFileSync('../data/articlesUrl.json'))
+  for (let article of file) {
+    await setTimeout(1000)
+    const html = await getPage(article.url)
+    getItems(html.data, false)
+  }
+}
+const sitemap = await getPage(SITE_MAP)
+
+getItems(sitemap.data, true);
+getArticle()
+
+
---
+++ src/utils.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import axios from 'axios';
+
+export const slugify = (string) => {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìıİłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string.toString().toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
+export const checkFileExist = async (path) => {
+  return fs.existsSync(path);
+}
+export const getPage = async (url) => {
+  return await axios.get(url)
+}
+export const handleFileJSON = async (path, content) => {
+  if (!checkFileExist(path)) fs.createWriteStream(path);
+  fs.writeFileSync(path, JSON.stringify(content))
+}
